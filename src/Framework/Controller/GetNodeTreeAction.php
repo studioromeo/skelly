d243cb67c5f0467caf60c0964ce4cd1e984484c1
Robert Rhoades <robert.rhoades@boxuk.com>
2016-02-05 17:07:45 +0000

    Adds lang negotiation

--- src/Framework/Controller/GetNodeTreeAction.php
+++ src/Framework/Controller/GetNodeTreeAction.php
@@ -5,6 +5,7 @@ namespace RS\Framework\Controller;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr7Middlewares\Middleware\FormatNegotiator;
+use Psr7Middlewares\Middleware\LanguageNegotiator;
 use RS\TreeBuilder\TreeBuilderInterface;
 
 class GetNodeTreeAction
@@ -24,10 +25,16 @@ class GetNodeTreeAction
 
     public function dispatch(ServerRequestInterface $request, ResponseInterface $response) : ResponseInterface
     {
-        $query = $this->db->query('SELECT n.id, n.title, n.parent_id from nodes n
+
+        $lang = LanguageNegotiator::getLanguage($request);
+
+        $query = $this->db->prepare('SELECT n.id, n.title, n.parent_id from nodes n
 INNER JOIN node_attributes na ON na.node_id = n.id
 INNER JOIN attribute_values av on na.`attribute_value_id` = av.`id`
-where av.`attribute_value` = \'en\';');
+where av.`attribute_value` = :language;');
+
+        $query->bindParam(':language', $lang);
+        $query->execute();
 
         $data = $query->fetchAll(\PDO::FETCH_ASSOC);
 
@@ -36,16 +43,16 @@ where av.`attribute_value` = \'en\';');
 
         $contentType = $neg->getFormat($request);
         switch($contentType) {
-            case 'json':
-                $response->getBody()->write(json_encode($this->treeBuilder->build($data)));
-                break;
             case 'xml':
                 $xml = new \SimpleXMLElement('<root/>');
                 array_walk_recursive($this->treeBuilder->build($data), array($xml, 'addChild'));
                 $response->getBody()->write($xml->asXML());
                 break;
+            default:
+                $response->getBody()->write(json_encode($this->treeBuilder->build($data)));
+                break;
         }
 
-        return $response->withHeader('Content-Type', $contentType);
+        return $response->withoutHeader('Content-Type'); // Remove the Content-Type Header
     }
 }
--- src/middleware.php
+++ src/middleware.php
@@ -4,5 +4,6 @@ $container = $app->getContainer();
 // Middleware executed bottom to top
 $app->add(new \Psr7Middlewares\Middleware\ResponseTime());
 $app->add(new \Psr7Middlewares\Middleware\FormatNegotiator());
+$app->add(new \Psr7Middlewares\Middleware\LanguageNegotiator(['en', 'de', 'cn', 'jp']));
 //$app->add(new Psr7Middlewares\Middleware\AccessLog($container->get('logger')));
 //$app->add(new Psr7Middlewares\Middleware\ClientIp());
